55996724451c5b2d8a05b8ec25108592dc0582aa
bostonwilliamss1 <bostonwilliamss1@gmail.com>
2024-03-19 17:12:09 -0600

    Initial Commit

---
+++ tutorial/tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  tutorialApp.swift
+//  tutorial
+//
+//  Created by Boston Williams on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
